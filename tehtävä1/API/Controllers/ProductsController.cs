1b4db91713c6a087ba638f1740a4986f173e7ba5
Jonne Vuorela <jonne.vuorela@proton.me>
2025-10-21 14:37:33 +0300

    Tehtävä 1 valmis / CRUD toiminnot product controllerille

--- tehtävä1/API/Controllers/ProductsController.cs
+++ tehtävä1/API/Controllers/ProductsController.cs
@@ -20,5 +20,84 @@ public class ProductsController : ControllerBase
             return NotFound(e.Message);
         }
     }
+
+    [HttpGet("{id}", Name = "GetProductById")]
+    public async Task<ActionResult<AppProduct>> GetProductById(long id)
+    {
+        try
+        {
+            var product = await AppProduct.GetById(id);
+            if (product == null)
+            {
+                return NotFound("Product not found");
+            }
+            return Ok(product);
+        }
+        catch (Exception ex)
+        {
+            return Problem($"Database error: {ex.Message}", statusCode: StatusCodes.Status500InternalServerError);
+        }
+    }
+
+    [HttpPost(Name = "AddProduct")]
+    public async Task<ActionResult<AppProduct>> AddNewProduct(string name)
+    {
+        try
+        {
+            var success = await AppProduct.Add(name);
+            if (!success)
+            {
+                return Problem($"error creating product", statusCode: StatusCodes.Status500InternalServerError);
+            }
+
+            return Ok(success);
+        }
+        catch (Exception ex)
+        {
+            return Problem(ex.Message, statusCode: StatusCodes.Status500InternalServerError);
+        }
+    }
+
+
+    [HttpPatch("{id}", Name = "UpdateProductById")]
+    public async Task<ActionResult<AppProduct>> UpdateProduct(long id, string name)
+    {
+        try
+        {
+
+            var success = await AppProduct.Update(name, id);
+            if (!success)
+            {
+                return NotFound();
+            }
+
+            return Ok(success);
+
+        }
+        catch (Exception ex)
+        {
+            return Problem($"Database error: {ex.Message}", statusCode: StatusCodes.Status500InternalServerError);
+        }
+    }
+
+    [HttpDelete("{id}", Name = "RemoveProductById")]
+    public async Task<ActionResult> RemoveProduct(long id)
+    {
+        try
+        {
+            bool removed = await AppProduct.Remove(id);
+
+            if (removed)
+            {
+                return NoContent();
+            }
+
+            return NotFound("product not found");
+        }
+        catch (Exception ex)
+        {
+            return Problem($"Database error: {ex.Message}", statusCode: StatusCodes.Status500InternalServerError);
+        }
+    }
 }
 
